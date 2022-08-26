4e83e186d53b7b3466f6ecdb05821d671bc76733
René <info@rg-engineering.eu>
2022-08-26 18:46:17 +0200

    initial

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"semp adapter settings": {
+		"en": "Adapter settings for semp",
+		"de": "Adaptereinstellungen für semp",
+		"ru": "Настройки адаптера для semp",
+		"pt": "Configurações do adaptador para semp",
+		"nl": "Adapterinstellingen voor semp",
+		"fr": "Paramètres d'adaptateur pour semp",
+		"it": "Impostazioni dell'adattatore per semp",
+		"es": "Ajustes del adaptador para semp",
+		"pl": "Ustawienia adaptera dla semp",
+		"zh-cn": "semp的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "вариант 2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
---
+++ main.js
@@ -0,0 +1,185 @@
+"use strict";
+
+/*
+ * Created with @iobroker/create-adapter v2.1.1
+ */
+
+// The adapter-core module gives you access to the core ioBroker functions
+// you need to create an adapter
+const utils = require("@iobroker/adapter-core");
+
+const Gateway = require("./lib/semp/Gateway").Gateway;
+
+
+/* to do
+ * uuid generieren und speichern
+ * ip holen
+ * sempport einstellbar in admin
+ * dummydevice entfernen
+ * devices bei start holen und anlegen
+ * neues device im admin anlegen und löschen
+ * typen in auswahl (z.Bsp. fridge)
+
+
+*/
+
+class Semp extends utils.Adapter {
+
+	
+
+	/**
+	 * @param {Partial<utils.AdapterOptions>} [options={}]
+	 */
+	constructor(options) {
+		super({
+			...options,
+			name: "semp",
+		});
+		this.on("ready", this.onReady.bind(this));
+		this.on("stateChange", this.onStateChange.bind(this));
+		// this.on("objectChange", this.onObjectChange.bind(this));
+		// this.on("message", this.onMessage.bind(this));
+		this.on("unload", this.onUnload.bind(this));
+
+		this.gw = null;
+		this.DummyDeviceUpdateIntervalID = null;
+		
+	}
+
+	/**
+	 * Is called when databases are connected and adapter received configuration.
+	 */
+	async onReady() {
+
+		try {
+			let uuid = "290B3891-0311-4854-4333-7C70BC802C2D";
+			let ip = "192.168.3.51";
+			let sempPort = 9765;
+			let name = "ioBroker Gateway";
+			let manufacturer = "ioBroker";
+
+			if (!ip) {
+				this.log.error("IP not specified!");
+			}
+			else {
+
+				this.gw = new Gateway(this, uuid, ip, sempPort, name, manufacturer);
+				if (this.gw != null) {
+					await this.gw.start();
+					this.log.debug("Started all!");
+
+					//=================================
+					//just for test:
+					this.gw.addDummyDevice();
+
+					this.DummyDeviceUpdateIntervalID = setInterval(() => this.UpdateDummyDevice, 10 * 60 * 1000);
+					//=================================
+
+				}
+			}
+
+			/*
+			// examples for the checkPassword/checkGroup functions
+			let result = await this.checkPasswordAsync("admin", "iobroker");
+			this.log.info("check user admin pw iobroker: " + result);
+	
+			result = await this.checkGroupAsync("admin", "admin");
+			this.log.info("check group user admin group admin: " + result);
+			*/
+		} catch (e) {
+			this.log.error("exception in onReady [" + e + "]");
+		}
+	}
+
+	UpdateDummyDevice() {
+		this.gw.UpdateDummyDevice();
+    }
+
+
+	/**
+	 * Is called when adapter shuts down - callback has to be called under any circumstances!
+	 * @param {() => void} callback
+	 */
+	onUnload(callback) {
+		try {
+			// Here you must clear all timeouts or intervals that may still be active
+			// clearTimeout(timeout1);
+			// clearTimeout(timeout2);
+			// ...
+			// clearInterval(interval1);
+
+			if (this.DummyDeviceUpdateIntervalID != null) {
+				clearInterval(this.DummyDeviceUpdateIntervalID);
+            }
+
+			if (this.gw != null) {
+				this.gw.stop();
+			}
+			callback();
+		} catch (e) {
+			callback();
+		}
+	}
+
+	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
+	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
+	// /**
+	//  * Is called if a subscribed object changes
+	//  * @param {string} id
+	//  * @param {ioBroker.Object | null | undefined} obj
+	//  */
+	// onObjectChange(id, obj) {
+	// 	if (obj) {
+	// 		// The object was changed
+	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
+	// 	} else {
+	// 		// The object was deleted
+	// 		this.log.info(`object ${id} deleted`);
+	// 	}
+	// }
+
+	/**
+	 * Is called if a subscribed state changes
+	 * @param {string} id
+	 * @param {ioBroker.State | null | undefined} state
+	 */
+	onStateChange(id, state) {
+		if (state) {
+			// The state was changed
+			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+		} else {
+			// The state was deleted
+			this.log.info(`state ${id} deleted`);
+		}
+	}
+
+	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
+	// /**
+	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
+	//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
+	//  * @param {ioBroker.Message} obj
+	//  */
+	// onMessage(obj) {
+	// 	if (typeof obj === "object" && obj.message) {
+	// 		if (obj.command === "send") {
+	// 			// e.g. send email or pushover or whatever
+	// 			this.log.info("send command");
+
+	// 			// Send response in callback if required
+	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
+	// 		}
+	// 	}
+	// }
+
+}
+
+if (require.main !== module) {
+	// Export the constructor in compact mode
+	/**
+	 * @param {Partial<utils.AdapterOptions>} [options={}]
+	 */
+	module.exports = (options) => new Semp(options);
+} else {
+	// otherwise start the instance directly
+	new Semp();
+}
